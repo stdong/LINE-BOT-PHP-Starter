a4099d24511cb3d62db9e7be93ed68d7069b4e76
Sarawut Tapkwa <chichi_dong@hotmail.com>
2017-11-21 14:25:28 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -52,9 +52,9 @@ if (!is_null($events['events'])) {
 			];
 
 			$messages_3 = [
-    "type": "image",
-    "originalContentUrl": "https://cdn.pixabay.com/photo/2017/11/16/08/35/box-2953722_960_720.jpg",
-    "previewImageUrl": "https://images.pexels.com/photos/654671/pexels-photo-654671.jpeg"
+    'type': 'image',
+    'originalContentUrl': 'https://cdn.pixabay.com/photo/2017/11/16/08/35/box-2953722_960_720.jpg',
+    'previewImageUrl': 'https://images.pexels.com/photos/654671/pexels-photo-654671.jpeg'
 ];
 
 
