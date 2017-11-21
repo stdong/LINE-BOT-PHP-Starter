d8c5ea6ffc2b51fc62c68ed7879244dbcdf51ec8
Sarawut Tapkwa <chichi_dong@hotmail.com>
2017-11-21 11:25:02 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,5 @@
 <?php
 $access_token = '1wPzgok0V+INNx5+eMuBL7myc0FZGfhCPAdiy5VrJy/yguDQbgRhWoceBEBCnxo0yWs/Aj33C/Ddm5V6qKvD9/Mz4qw6TaFtdBWbzWiwLgk6qPkvCQ46eSwmBEATMUahXbEy97sl27H97JLsmwBR0QdB04t89/1O/w1cDnyilFU=';
-
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
@@ -16,11 +15,42 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
+
+
+			if($text == "สวัสดี"){ 
+			  // Build message to reply back
+				$messages = [
+					'type' => 'text',
+					'text' => "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId']
+				];
+			}else if($text == "ชื่ออะไร"){
+			  // Build message to reply back
+				$messages = [
+					'type' => 'text',
+					'text' => "ฉันยังไม่มีชื่อนะ"
+				];
+			}else if($text == "ทำอะไรได้บ้าง"){ 
+			  // Build message to reply back
+				$messages = [
+					'type' => 'text',
+					'text' => "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ"
+				];
+			}else{
+			  // Build message to reply back
+				$messages = [
+					'type' => 'text',
+					'text' => "ฉันไม่เข้าใจคำสั่ง "+$text
+				];
+			}
+ 
+			
 			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
+			//$messages = [
+			//	'type' => 'text',
+			//	'text' => $text
+			//];
+
+
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
@@ -30,7 +60,6 @@ if (!is_null($events['events'])) {
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -39,7 +68,6 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
 			curl_close($ch);
-
 			echo $result . "\r\n";
 		}
 	}
