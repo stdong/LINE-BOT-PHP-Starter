41c897b71bfaa294d654beb58b204eec0b3d4a9d
Sarawut Tapkwa <chichi_dong@hotmail.com>
2017-11-21 14:23:39 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -51,6 +51,11 @@ if (!is_null($events['events'])) {
 				'text' => $text
 			];
 
+			$messages_3 = [
+    "type": "image",
+    "originalContentUrl": "https://cdn.pixabay.com/photo/2017/11/16/08/35/box-2953722_960_720.jpg",
+    "previewImageUrl": "https://images.pexels.com/photos/654671/pexels-photo-654671.jpeg"
+];
 
 
 			// Make a POST Request to Messaging API to reply to sender
@@ -58,7 +63,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages, $messages_2],
+				'messages' => [$messages, $messages_2, $messages_3],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
